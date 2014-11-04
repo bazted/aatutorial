6247b5b2b78f9ac46616ab5cb3c75f28d2c2e2d1
bazted <bazushun.taras@gmail.com>
2014-11-04 20:56:20 +0100

    basic configuration

---
+++ AATutorial/app/src/main/java/com/pgs/bazted/aatutorial/usual/ChildActivityWithExtra.java
@@ -0,0 +1,18 @@
+package com.pgs.bazted.aatutorial.usual;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by bazted on 04/11/14.
+ */
+public class ChildActivityWithExtra extends FragmentActivity {
+    public static final String STRING_EXTRA_KEY = "STRING_EXTRA_KEY";
+
+    public static void startChild(Context context, String stringExtra) {
+        Intent intent = new Intent(context, ChildActivityWithExtra.class);
+        context.startActivity(intent);
+    }
+
+}
